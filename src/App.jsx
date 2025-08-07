cd56939393abf99a419e144aab78bca3a5898bb1
SylviaRemington <sylviaremington@gmail.com>
2025-08-07 14:27:20 -0700

    exporting and importing MailboxList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
+import MailboxList from './components/MailboxList/MailboxList';
 
 const App = () => {
   return <h1>Hello world!</h1>;
