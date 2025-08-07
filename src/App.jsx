5e49247dedeb6e01b638b0e55f09230519f6f216
SylviaRemington <sylviaremington@gmail.com>
2025-08-07 14:30:44 -0700

    exporting and importing MailboxDetails from MailboxDetails.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import MailboxList from './components/MailboxList/MailboxList';
 import MailboxForm from './components/MailboxForm/MailboxForm';
-
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 
 const App = () => {
   return <h1>Hello world!</h1>;
