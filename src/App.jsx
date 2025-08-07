447b5c57ca9995d73177952bbddb4d3df07ad96e
SylviaRemington <sylviaremington@gmail.com>
2025-08-07 14:20:17 -0700

    creating NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 // src/App.jsx
+import { useState } from 'react';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
   return <h1>Hello world!</h1>;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,5 +2,18 @@
 // ➜  react-router-dom-lab git:(main) mkdir src/components/NavBar 
 // ➜  react-router-dom-lab git:(main) touch src/components/NavBar/NavBar.jsx
 
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>MailboxList</li>
+        <li>MailboxDetails</li>
+        <li>MailboxForm</li>
+      </ul>
+    </nav>
+  );
+};
 
+export default NavBar;
 
