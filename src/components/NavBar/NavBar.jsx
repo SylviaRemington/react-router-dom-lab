06115dfc0398794f3dc99e4fdf71ce855d4e2476
SylviaRemington <sylviaremington@gmail.com>
2025-08-09 16:17:13 -0700

    adding links to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,29 @@
 // ➜ react-router-dom-lab git:(main) mkdir src/components
 // ➜  react-router-dom-lab git:(main) mkdir src/components/NavBar
 // ➜  react-router-dom-lab git:(main) touch src/components/NavBar/NavBar.jsx
-import '../../App.css';
-import "../../App.jsx";
-// import { Link } from "react-router-dom";
+// import '../../App.css';
+// import "../../App.jsx";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
-        <li>*Home*</li>
-        <li>*Mailboxes(MailboxList)*</li>
-        <li>*New Mailbox(MailboxForm)*</li>
-        <li>*New Letter(MailboxDetails)*</li>
+        <li>
+          <Link to="/">*Home*</Link>
+        </li>
+
+        <li>
+          <Link to="/mailboxes">*Mailboxes(MailboxList)*</Link>
+        </li>
+
+        <li>
+          <Link to="/new-mailbox">*New Mailbox(MailboxForm)*</Link>
+        </li>
+
+        {/* <li>
+          <Link to="/mailboxes/:mailboxId">*New Letter(MailboxDetails)*</Link>
+        </li> */}
       </ul>
     </nav>
   );
