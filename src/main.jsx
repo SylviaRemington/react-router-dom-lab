c480151de500eb98cafe3c6b837f1c49f3e75939
SylviaRemington <sylviaremington@gmail.com>
2025-08-09 00:12:24 -0700

    notes on the order of things and the order in which things happen within the app starting with index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 //This is the file that uses JS to render all content into the index.html div called root.
-
+// When app starts, it starts with index.html & then runs main.jsx. Then it loads index.css. And then it shows App
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
