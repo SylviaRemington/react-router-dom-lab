57cc72a78424f8d40e389dee1713f02420db7aef
SylviaRemington <sylviaremington@gmail.com>
2025-08-09 20:59:04 -0700

    correcting syntax for MailboxList.jsx line 14

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -1,22 +1,24 @@
 // Creating basic looping through mailbox list via map function
-import '../../App.css';
+import "../../App.css";
+import { Link } from "react-router";
 
 const MailboxList = (props) => {
-    console.log(props.mailboxes);
-    return (
-        // rendering everything out here
-        <>
-        <h2>Mailbox List of all the fabulous mailboxes 📫 🎉🥳 🙌 </h2>
-        <ul>
-            {props.mailboxes.map((currentMailbox) => (
-                <li key={currentMailbox.boxOwnerName}>{currentMailbox.boxOwnerName}</li>
-            ))}
-        </ul>
-        </>
-    );
+  console.log(props.mailboxes);
+  return (
+    // rendering everything out here
+    <>
+      <h2>Mailbox List of all the fabulous mailboxes 📫 🎉🥳 🙌 </h2>
+      <ul>
+        {props.mailboxes.map((currentMailbox) => (
+          <li key={currentMailbox.boxOwnerName}>
+            <Link to={`/mailboxes/${currentMailbox._id}`}>
+              {currentMailbox.boxOwnerName}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-
-
 export default MailboxList;
-
