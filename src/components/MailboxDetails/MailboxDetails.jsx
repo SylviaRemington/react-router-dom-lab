c66f9a84fde302981e720eba4b87387ff32e4e12
SylviaRemington <sylviaremington@gmail.com>
2025-08-10 01:24:32 -0700

    adding addtl notes - so that when come back to this lab I can review and understand better if need be

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -5,15 +5,18 @@ import { useParams } from "react-router";
 const MailboxDetails = (props) => {
     // getting the mailboxId from the URL (I'm using 'id' to match the Route path '/mailboxes/:id')
     const { id } = useParams();
+
+    // keeping console.log details to keep track of what I did - for future reference when I review old labs.
     // seeing the difference of what happens in console log with params and params.id
     // console.log('Params are: ', params, params.id);
 
     // finding the mailbox with the matching id (as _id)
-    const singleMailbox = props.mailboxes.find(
+    const singleMailbox = props.mailboxes.find( //using find to search the mailboxes array for a mailbox with _id (where _id = the id from the URL)
+        // using Number(id) to convert the id (which is a string in the url) to a number so that it matches the _id in my mailboxes array
         (mailbox) => mailbox._id === Number(id)
     );
 
-    // If no mailbox is found, display error message of 'Mailbox Not Found!'
+    // If no mailbox is found (e.g. is undefined), display error message of 'Mailbox Not Found!'
     if (!singleMailbox) {
         return (
             <>
