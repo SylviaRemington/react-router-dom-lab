1b62d41b217075fe3a173188f1e5e61127ce433c
SylviaRemington <sylviaremington@gmail.com>
2025-08-10 02:42:05 -0700

    adding functionality for MailboxList as per user stories

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -4,16 +4,22 @@ import { Link } from "react-router";
 
 const MailboxList = (props) => {
   console.log(props.mailboxes);
+
   return (
     // rendering everything out here
     <>
       <h2>Mailbox List of all the fabulous mailboxes 📫 🎉🥳 🙌 </h2>
       <ul>
         {props.mailboxes.map((currentMailbox) => (
-          <li key={currentMailbox.boxOwnerName}>
-            <Link to={`/mailboxes/${currentMailbox._id}`}>
-              {currentMailbox.boxOwnerName}
-            </Link>
+          // <li key={currentMailbox.boxOwnerName}>
+          <li key={currentMailbox._id}>
+            <div className="mail-box">
+              <Link to={`/mailboxes/${currentMailbox._id}`}>
+                {/* {currentMailbox.boxOwnerName} */}
+                {/* The user story says: “Each mailbox in the list should be a square container with a box number _id.” */}
+                Box #{currentMailbox._id}
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
