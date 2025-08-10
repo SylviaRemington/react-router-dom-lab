3756889d08ebaa0cf7e1a6070a1d078679fdb185
SylviaRemington <sylviaremington@gmail.com>
2025-08-09 22:58:47 -0700

    adding addtl content to MailboxDetails & adding a MailboxDetails Route in App.jsx

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -4,14 +4,15 @@ const MailboxDetails = (props) => {
     return (
         <>
             <h2>Mailbox Details Page</h2>
-            {props.MailboxDetails}
+            {/* {props.MailboxDetails} */}
+            <ul>
+                <li>Mailbox Size: </li>
+                <li>Mailbox Owner Name: </li>
+            </ul>
         </>
 
     )
 }
-
-
-
 
 
 export default MailboxDetails;
