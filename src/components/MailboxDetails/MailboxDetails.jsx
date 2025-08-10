1aec5cb274467393cb908badc35dbceb27b74b0a
SylviaRemington <sylviaremington@gmail.com>
2025-08-10 00:36:28 -0700

    adding notes of params and the simplicity of using params

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,7 +1,14 @@
+// getting access to the params -- just need to import this and then do the const params=useParams() - then have access to them in React
 import { useParams } from "react-router";
 
 // We are using props to have data being passed into it, so that we can have individual details about the mailboxes.
 const MailboxDetails = (props) => {
+    const params = useParams();
+
+    console.log('Params are: ', params);
+
+
+
     return (
         <>
             <h2>Mailbox Details Page</h2>
